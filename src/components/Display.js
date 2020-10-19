5f94427d43712750c035abc1b3e7d7a08af6ee79
Kole Gasior <jkole822@gmail.com>
2020-10-19 17:08:39 -0400

    minor changes

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import History from './History';
-import Input from './Input';
+import React from "react";
+import History from "./History";
+import Input from "./Input";
 
 const Display = () => {
 	return (
-		<div id='calcDisplay' className='ui right aligned segment'>
+		<div id="calcDisplay" className="ui right aligned segment">
 			<History />
 			<Input />
 		</div>
--- src/components/Input.js
+++ src/components/Input.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React from "react";
+import { connect } from "react-redux";
 
 class Input extends React.Component {
 	render() {
-		return <div id='display'>{this.props.selectInput}</div>;
+		return <div>{this.props.selectInput}</div>;
 	}
 }
 
